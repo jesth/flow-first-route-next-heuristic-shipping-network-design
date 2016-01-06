265972801bf92a03b818c10b270970b662776f05
Jesper Thorsen <thorsen1990@gmail.com>
2016-01-06 10:43:41 +0100

    Yet another change.

--- Speciale/src/GitTest.java
+++ Speciale/src/GitTest.java
@@ -3,7 +3,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	    System.out.println("Git is difficult");
+	    System.out.println("Git is now working");
 	}
 
 }
