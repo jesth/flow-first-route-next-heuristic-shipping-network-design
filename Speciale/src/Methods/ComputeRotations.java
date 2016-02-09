0ed93e7a31853b89ff297353f9ca0549407df460
Alexander Krogsgaard <alexander@krogsgaard.biz>
2016-02-09 13:05:45 +0100

    working on calculating profit by inserting a port in a rotation

--- Speciale/src/Methods/ComputeRotations.java
+++ Speciale/src/Methods/ComputeRotations.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import Data.Demand;
 import Data.DistanceElement;
 import Data.Port;
+import Data.VesselClass;
 import Graph.Edge;
 import Graph.Graph;
 import Results.Result;
@@ -34,6 +35,7 @@ public class ComputeRotations {
 
 	public static void insertBestPort(Rotation rotation){
 		ArrayList<DistanceElement> distances = new ArrayList<DistanceElement>();
+		ArrayList<Edge> edges = new ArrayList<Edge>();
 		double loadFactor = 0;
 		for(Edge e : rotation.getRotationEdges()){
 			if(e.isSail()){
@@ -41,21 +43,58 @@ public class ComputeRotations {
 				//TODO hardcoded 95%
 				if(loadFactor < 0.95){
 					distances.add(e.getDistance());
+					edges.add(e);
 				}
 			}
 		}
 		
+		int bestProfit = -Integer.MAX_VALUE;
+		Port bestPort = null;
+		Edge bestEdge = null;
 		for(Port p : graph.getData().getPorts().values()){
-			
+			for(Edge e : edges){
+				DistanceElement d = e.getDistance();
+				int detour = getDetour(d, p);
+				if(detour < (double) d.getDistance()*0.2){
+					int profit = calcPortInsertProfit(rotation, p, e);
+					if(profit > bestProfit){
+						bestProfit = profit;
+						bestPort = p;
+						bestEdge = e;
+					}
+				}
+			}
 		}
-		
-		
-		ArrayList<Demand> demands = graph.getData().getDemands();
-		
-		
-		ArrayList<Port> rotationPorts = rotation.getPorts();
+		//TODO actually insert port
 	}
 
+	public static int calcPortInsertProfit(Rotation rotation, Port insertPort, Edge edge){
+		Port fromPort = edge.getFromNode().getPort();
+		Port toPort = edge.getToNode().getPort();
+		Demand toInsert = graph.getData().getDemand(fromPort.getPortId(), insertPort.getPortId());
+		Demand fromInsert = graph.getData().getDemand(insertPort.getPortId(), toPort.getPortId());
+		
+		int roomLeft = edge.getCapacity() - edge.getLoad();
+		
+		int profit = 0;
+		profit += Math.min(toInsert.getDemand(), roomLeft) * toInsert.getRate();
+		profit += Math.min(fromInsert.getDemand(), roomLeft) * fromInsert.getRate();
+		
+		VesselClass v = rotation.getVesselClass();
+		
+		int detourDays = (int) ((getDetour(edge.getDistance(), insertPort) / v.getDesignSpeed())/ 24.0);
+		
+		int fuelSail = detourDays * (int) v.getFuelConsumptionDesign();
+		int fuelDwell = (int) v.getFuelConsumptionIdle();
+		int TCCost = detourDays * v.getTCRate();
+		int fixedCall = insertPort.getFixedCallCost();
+		int varCall = insertPort.getVarCallCost() * v.getCapacity();
+
+		profit -= (fuelSail + fuelDwell + TCCost + fixedCall + varCall);
+		
+		return profit;
+	}
+	
 	public static int getDetour(DistanceElement currentLeg, Port addPort){
 		Port port1 = currentLeg.getOrigin();
 		Port port2 = addPort;
